d3575e6b060d5ed225e233622443ad1f7f9a254f
Nicholj25 <Nicholj25@gmail.com>
2018-05-05 14:00:34 -0400

    Update Critter.hpp

--- Critter.hpp
+++ Critter.hpp
@@ -4,7 +4,11 @@
 class Critter
 {
     protected:
+        int xLocation;      //Current x location on the board
+        int yLocation;      //Current y location on the board
+        char gameBoardCharacter;
     public:
         virtual void Move() = 0;
+        char getCritterType();
 };
 #endif
